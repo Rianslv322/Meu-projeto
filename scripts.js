23c6e6a917b82779f660f57631225e3ae9207668
Rianslv322 <riansilvasouza915@gmail.com>
2025-11-06 09:07:16 -0300

    novas atualizaç~poes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-document.getElementsByName("input")
+let lista = []
+
+function Salvar() {
+    let entrada1 = document.getElementById("nome").value
+    let entrada2 = document.getElementById("Gmail").value
+    let entrada3 = document.getElementById("senha").value
+
+    lista = [entrada1, entrada2, entrada3]
+
+    console.log(lista)
+    alert("Dados salvos: " + lista)
+}
