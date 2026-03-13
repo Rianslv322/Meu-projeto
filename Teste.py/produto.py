fcca3c7f5d61d7a48662e500f6e0307b7fc0194b
Rianslv322 <riansilvasouza915@gmail.com>
2026-03-13 12:06:06 -0300

    atualização do Dict

--- Teste.py/produto.py
+++ Teste.py/produto.py
@@ -6,4 +6,27 @@ class Produto:
         self.quantidade = quantidade_estoque
         
     
-    def exibir_produtos(self)
+    def exibir_produtos(self):
+        print(f"""
+| ID: {self.id_produto}
+| Nome: {self.nome}
+| Preço: {self.preco}
+|Quantidade: {self.quantida}""")
+        
+    def adicionar_Estoque(self):
+        nome = input("Nome do Produto: ")
+        qtdd = int(input("Quantidade: "))
+        
+        self.quantidade = {nome: f"Quantidade:{qtdd} "}
+        print("Produtos adicionados")
+        
+        for nome_produto, quantidade_produto in self.quantidade.items():
+            print(f"""
+|{nome_produto}
+|{quantidade_produto}
+""")
+        
+        
+        
+p1 = Produto(12,"banana",1.5,6)
+p1.adicionar_Estoque()
